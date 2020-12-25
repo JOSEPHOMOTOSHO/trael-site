0710290b4bf68c618ad2daf4b94a9064958f4a72
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2020-12-25 01:52:33 +0100

    master updates

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,35 +1,13 @@
 import "../styles/style.css"
-<<<<<<< HEAD
-import Person from "./modules/Person"
-=======
 import MobileMenu from "./Modules/MobileMenu"
 
 let mobileMenu = new MobileMenu();
->>>>>>> header
 
 if(module.hot){
     module.hot.accept()
 }
 
-<<<<<<< HEAD
-/* PrACTICING CONSTRUCTORS AND OBJECT ORIENTED PROGRAMMING */
-
-class Adult extends Person{
-    paytaxes(){
-        console.log(this.name + "DOESNT OWE ANYMORE TAXES")
-    }
-}
-
-
-let Joseph = new Person("Joseph","black");
-Joseph.greet()
-
-let Josephine = new Adult("Josephine","pink");
-Josephine.greet()
-Josephine.paytaxes();
-=======
 
 
 
->>>>>>> header
 
